1dcd176a7aa4675fe1fae4c9a557a029f0d16177
bert-pixel <bert@socialbrothers.nl>
2026-09-14 16:13:38 +0200

    Add toast notification component and enhance form styles

--- js/main.js
+++ js/main.js
@@ -42,10 +42,24 @@ const Flipcards = () => {
   });
 };
 
+const SearchMenu = () => {
+  const searchToggle = document.querySelector(".js-search-main-toggle");
+  const searchContainer = document.querySelector(".js-search-main-container");
+  const searchOverlay = searchContainer.querySelector(".search-main__overlay");
+  [searchToggle, searchOverlay].forEach((item) =>
+    item.addEventListener("click", () => {
+      searchContainer.classList.toggle("open");
+    }),
+  );
+};
+
 const MobMenu = () => {
   const mobMenu = document.querySelector(".js-mobnav");
   const backBtn = mobMenu.querySelector(".js-back");
+  const searchBtn = mobMenu.querySelector(".js-search-toggle");
+  const searchSubnav = mobMenu.querySelector(".js-nav-search");
   const toggles = mobMenu.querySelectorAll(".js-toggle");
+  let prevSubnav = null;
 
   const closeSubnav = () => {
     mobMenu
@@ -53,12 +67,27 @@ const MobMenu = () => {
       .forEach((navItem) => navItem.classList.remove("open"));
   };
 
+  // Bind events
   backBtn.addEventListener("click", closeSubnav);
+
+  searchBtn.addEventListener("click", () => {
+    searchSubnav.classList.toggle("open");
+    console.log(searchSubnav);
+  });
 
   toggles.forEach((toggle) => {
     toggle.addEventListener("click", (event) => {
+      if (prevSubnav) prevSubnav.classList.remove("open");
+
       const subNav = toggle.closest(".js-container").querySelector(".js-nav");
+      if (prevSubnav == subNav) {
+        prevSubnav = null;
+        return;
+      }
       subNav.classList.toggle("open");
+
+      // Only tag a card level nav for closing
+      if (subNav.classList.contains("card__items")) prevSubnav = subNav;
     });
   });
 };
@@ -66,7 +95,7 @@ const MobMenu = () => {
 const LinkExpand = () => {
   document.querySelectorAll("a[type]").forEach((link) => {
     if (link.getAttribute("type") === "tel") {
-      link.href = `tel:${link.href.replace("http://", "")}`;
+      link.href = `tel:${link.href.replace(/http:\/\/|%20|\//g, "")}`;
     }
   });
 };
@@ -77,21 +106,50 @@ const Filter = () => {
 
   filters.forEach((filter) => {
     const target = document.querySelector("#" + filter.dataset.for);
+    const clear = filter.closest(".filter").querySelector(".js-clear");
     const wiperSlides = target.querySelectorAll(".swiper-slide");
+
+    const $swiper = jQuery(`#${filter.dataset.for} .swiper`);
+    let swiperInstance = null;
+
     let query = "";
 
+    const updateSwiper = () => {
+      swiperInstance.update();
+      swiperInstance.slideTo(0, 0);
+    };
+
+    const reset = () => {
+      filter.value = "";
+      filter.select();
+      wiperSlides.forEach(
+        (wiperSlide) => (wiperSlide.style.display = "block"),
+        (clear.style.display = "none"),
+      );
+      updateSwiper();
+    };
+
+    // Listen to reset
+    clear.addEventListener("click", () => {
+      reset();
+    });
+
+    // Listen to input
     filter.addEventListener("input", () => {
-      console.log(filter.value);
       query = filter.value;
 
+      // Initialize the swiperInstance once
+      if (!swiperInstance) {
+        swiperInstance = $swiper.data("swiper");
+      }
+
       if (query.length === 0) {
-        wiperSlides.forEach(
-          (wiperSlide) => (wiperSlide.style.display = "block"),
-        );
+        reset();
         return;
       }
       if (query.length < 3) return;
       const queryLowercase = query.toLowerCase();
+
       wiperSlides.forEach((wiperSlide) => {
         // Hide all slides first
         wiperSlide.style.display = "none";
@@ -103,6 +161,21 @@ const Filter = () => {
             .indexOf(queryLowercase) > -1;
         if (match) wiperSlide.style.display = "block";
       });
+
+      clear.style.display = "block";
+      updateSwiper();
+    });
+  });
+};
+
+const Toast = () => {
+  const toasts = document.querySelectorAll(".toast");
+
+  toasts.forEach((toast) => {
+    const toastCloseBtn = toast.querySelector(".js-close");
+
+    toastCloseBtn.addEventListener("click", () => {
+      toast.classList.add("hidden");
     });
   });
 };
@@ -118,6 +191,8 @@ const init = () => {
   Flipcards();
   LinkExpand();
   Filter();
+  SearchMenu();
+  Toast();
   window.addEventListener("scroll", ScrollToCss);
 };
 
